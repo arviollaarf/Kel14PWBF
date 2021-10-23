6dbd22d022686dcc9961a57a98c12e08eb87ce04
arviollaarf <arviollaff@gmail.com>
2021-10-23 06:37:02 -0800

    comitt

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home',[
-        "title"=>"Home"
-    ]);
-});
+Route::get('/','App\Http\Controllers\HomeController@login');
+		
+
 
 Route::get('/data', function () {
     return view('data', [
